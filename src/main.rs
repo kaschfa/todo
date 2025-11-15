725997d88c5568e742e74a7f6c6c4329a9e556ee
Fabian Kaschner <kaschner.fab@gmail.com>
2025-11-15 13:23:48 +0100

    base structure changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
@@ -13,16 +10,35 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Stylesheet { href: CSS }
-        div { id: "container",
-            div { id: "sidebar",
+        Navbar{}
+        Content{}
+    }
+}
 
+#[component]
+fn Navbar() -> Element {
+    rsx! {
+        div { id: "navbar",
+            h1 {"Navbar"}
+        }
+    }
+}
+
+#[component]
+fn Content() -> Element {
+    rsx! {
+        div { id: "layout",
+            div { id: "sidebar",
+                h1 { "Sidebar" }
             }
-            div { id: "note",
+            div { id: "container",
                 div { id: "head",
                     h1 { "TitleBar" }
-                }
+                },
                 div { id: "todo",
-                    h1 { "Body" }
+                    textarea { id: "todo-text",
+                    "Type here"
+                    }
                 }
             }
         }
