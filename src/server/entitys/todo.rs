44203c1ea21beef9727128191b55e181c9b31606
Fabian Kaschner <kaschner.fab@gmail.com>
2025-12-04 16:18:46 +0100

    string formatting for time and date working

--- src/server/entitys/todo.rs
+++ src/server/entitys/todo.rs
@@ -3,6 +3,8 @@ use crate::shared::dto::*;
 use sea_orm::entity::prelude::*;
 use sea_orm::IntoActiveModel;
 use sea_orm::Set;
+use time::macros::format_description;
+use time::{Date, PrimitiveDateTime, Time};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
 #[sea_orm(table_name = "todo")]
@@ -13,7 +15,7 @@ pub struct Model {
     pub note: Option<String>,
     pub due_date: Date,
     pub due_time: Time,
-    pub created_at: DateTime,
+    pub created_at: PrimitiveDateTime,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
@@ -38,24 +40,35 @@ impl From<Model> for TodoDto {
 // Optional reverse transform if needed
 impl IntoActiveModel<ActiveModel> for TodoDto {
     fn into_active_model(self) -> ActiveModel {
+        let format_dt =
+            format_description!("[year]-[month]-[day] [hour]:[minute]:[second].[subsecond]");
+        let format_t = format_description!("[hour]:[minute]:[second].[subsecond]");
+        let format_d = format_description!("[year]-[month]-[day]");
+        dbg!(&self.due_time);
         ActiveModel {
             id: Set(self.id),
             title: Set(self.title),
             note: Set(self.note),
-            due_date: Set(self.due_date.parse().expect("invalid date")),
-            due_time: Set(self.due_time.parse().expect("invalid time")),
-            created_at: Set(self.created_at.parse().expect("invalid datetime")),
+            due_date: Set(Date::parse(&self.due_date.trim(), &format_d).expect("invalid date")),
+            due_time: Set(Time::parse(&self.due_time.trim(), &format_t).expect("invalid time")),
+            created_at: Set(
+                PrimitiveDateTime::parse(&self.created_at.trim(), &format_dt)
+                    .expect("invalid datetime"),
+            ),
         }
     }
 }
 
 impl NewTodoDto {
     pub fn into_active_model(self) -> ActiveModel {
+        let format_t = format_description!("[hour]:[minute]:[second].[subsecond]");
+        let format_d = format_description!("[year]-[month]-[day]");
+        dbg!(&self.due_time);
         ActiveModel {
             title: Set(self.title),
             note: Set(self.note),
-            due_date: Set(self.due_date.parse().expect("invalid date")),
-            due_time: Set(self.due_time.parse().expect("invalid time")),
+            due_date: Set(Date::parse(&self.due_date.trim(), &format_d).expect("invalid date")),
+            due_time: Set(Time::parse(&self.due_time.trim(), &format_t).expect("invalid time")),
             created_at: Default::default(), // Let DB handle it
             id: Default::default(),         // Auto-increment
         }
